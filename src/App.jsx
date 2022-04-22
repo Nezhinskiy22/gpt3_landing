5f8d96dce1376c4447199623b96793e5d627fc87
Nezhinskiy22 <nezhinskiy22@gmail.com>
2022-04-22 18:01:39 +0300

    navbar responsive

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import Blog from "./containers/blog/Blog";
+import Navbar from "./components/navbar/Navbar";
+import Header from "./containers/header/Header";
+import WhatGPT3 from "./containers/whatGPT3/WhatGPT3";
+import Possibility from "./containers/possibility/Possibility";
+import Feature from "./components/feature/Feature";
+import CTA from "./components/cta/CTA";
+import Footer from "./containers/footer/Footer";
+import Brand from "./components/brand/Brand";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Feature />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
